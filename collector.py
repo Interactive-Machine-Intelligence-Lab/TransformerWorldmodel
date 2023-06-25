f3430e8e544003935edac61a5f742b29370ee0b4
rzbsys <rzbsys@unist.ac.kr>
2023-06-26 00:32:40 +0900

    Fix Collect

--- collector.py
+++ collector.py
@@ -96,7 +96,7 @@ class Collector:
                         metrics_episode = {k: v for k, v in episode.compute_metrics().__dict__.items()}
                         metrics_episode['episode_num'] = episode_id
                         metrics_episode['action_histogram'] = wandb.Histogram(np_histogram=np.histogram(episode.actions.numpy(), bins=np.arange(0, self.env.num_actions + 1) - 0.5, density=True))
-                        to_log.append({f'{self.dataset.name}/{k}/agent{agent_id}': v for k, v in metrics_episode.items()})
+                        to_log.append({f'{self.datasets[agent_id].name}/agent{agent_id}/{k}': v for k, v in metrics_episode.items()})
                         returns.append(metrics_episode['episode_return'])
 
                 self.obs = self.env.reset()
